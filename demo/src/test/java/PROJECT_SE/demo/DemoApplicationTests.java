63f27fe6766c73bb7a9368bc0dc99a362e2ef6bb
toids19 <118681223+toids19@users.noreply.github.com>
2024-11-10 18:47:04 +0200

    Add files via upload

---
+++ demo/src/test/java/PROJECT_SE/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package PROJECT_SE.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
